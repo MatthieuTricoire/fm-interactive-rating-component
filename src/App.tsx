4c871b17403bb168ca42a30d461fdc2af8815946
MatthieuTricoire <matthieutricoire@gmail.com>
2023-04-27 09:10:29 +0200

    initialization

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Rating from "./components/Rating";
+
+function App() {
+  return (
+    <main>
+      <Rating />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Rating.tsx
@@ -0,0 +1,11 @@
+import style from "../components/rating.module.css";
+
+import { FC } from "react";
+
+interface RatingProps {}
+
+const Rating: FC<RatingProps> = ({}) => {
+  return <div>Rating component</div>;
+};
+
+export default Rating;
