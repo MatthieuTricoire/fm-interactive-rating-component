fd386fe64a90ac508dee23e9c87177dafaedcb96
MatthieuTricoire <matthieutricoire@gmail.com>
2023-05-02 08:07:25 +0200

    clean unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useContext } from "react";
 import "./App.css";
 import Rating from "./components/rating/Rating";
+import Thanks from "./components/thanks/Thanks";
+import { SubmitContext } from "./context/submit-ctx";
 
 function App() {
+  const { isSubmit, setIsSubmit } = useContext(SubmitContext);
+
   return (
     <main>
       <Rating />
+      {isSubmit}
+      <Thanks />
     </main>
   );
 }
--- src/components/thanks/Thanks.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-
-const Thanks: FC = () => {
-  return <div>Thanks</div>;
-};
-
-export default Thanks;
